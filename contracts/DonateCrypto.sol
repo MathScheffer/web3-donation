ad54cbe45f17da7cd9a4e0d7d830e7e1d48cb38d
MathScheffer <matheusmartinsscheffer@hotmail.com>
2025-04-13 23:28:10 -0300

    iniciar projeto

---
+++ contracts/DonateCrypto.sol
@@ -0,0 +1,43 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+struct Campaign {
+    address author;
+    string title;
+    string description;
+    string videoUrl;
+    string imageUrl;
+    uint256 balance;
+    bool active;
+}
+contract DonateCrypto {
+    //qualquer pessoa consegue acessar na blockchain
+    uint256 public fee = 100;//wei
+    uint256 public nextId = 0;
+
+    mapping(uint256 => Campaign) public campaings;
+    //calldata -> Dados temporários que não são escritos na blockchain, mas podem ser lidos eventualmente
+    function addCampaing(string calldata title, string calldata description, string calldata videoUrl, string calldata imageUrl) public{
+        Campaign memory newCampaing;
+        newCampaing.author = msg.sender;//o endereço do usuário que chamou a função
+        newCampaing.title = title; //campanha de mensagem recebida pelos parâmetros
+        newCampaing.description = description; 
+        newCampaing.videoUrl = videoUrl;//https://www.youtube.com/watch?v=06C-qZYLB4Q&list=PLWdR2nNXI53Ub8pGmj1uJEeFs7zgHxk9rD  
+        newCampaing.active = true;
+        nextId++;
+        campaings[nextId] = newCampaing;
+    }
+
+    function donate(uint256 id) public payable {
+        
+        require(campaings[id].active, "Campanha nao esta ativa!");
+        require(msg.value > 0, "Valor precisa ser maior que zero.");
+
+        campaings[id].balance += msg.value;
+    }
+
+    function deactivateCampaing(uint256 id) public {
+        campaings[id].active = false;
+    }
+}
